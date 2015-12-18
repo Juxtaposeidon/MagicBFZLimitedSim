2baf6ccc5f81dd9420c84bbdc4e2484cbbc319f8
dbcjckyee <dbcjckyee@gmail.com>
2015-12-17 21:31:21 -0800

    can remove user-selected cards from packs

--- app/controllers/draft_controller.rb
+++ app/controllers/draft_controller.rb
@@ -2,15 +2,16 @@ class DraftController < ApplicationController
   def new
     session[:cards] = []
         p session[:cards]
-
-    @newdraft = Draft.new
-    @startingpack = @newdraft.player1.pack1.contents
-    @cpu = @newdraft.cpuplayers
+    @@newdraft = Draft.new
+    @startingpack = @@newdraft.player1.pack1.contents
+    @cpu = @@newdraft.cpuplayers
   end
 
   def addcard
-    session[:cards] << Card.find(params['id'])
-    p session[:cards]
+    @@newdraft.player1.addPool(Card.find(params['id']))
+
+    p @@newdraft.player1.pool
+    @@newdraft.player1.removeCard(Card.find(params['id']))
   end
 
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,8 +1,22 @@
 class Player
-  attr_reader :pack1, :pack2, :pack3
+  attr_reader :pack1, :pack2, :pack3, :pool
   def initialize
+    @pool = []
     @pack1 = Pack.new
     @pack2 = Pack.new
     @pack3 = Pack.new
+  end
+
+  def addPool(card)
+    @pool << card
+  end
+
+  def removeCard(card)
+    p @pool.length
+    if @pool.length <= 14
+      @pack1.contents.delete_if {|item| item == card}
+    end
+    p @pack1.contents.length
+    p @pack1.contents
   end
 end
