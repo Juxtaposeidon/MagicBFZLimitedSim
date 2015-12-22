a1941700252be01ff43995b81704bf12d2253005
dbcjckyee <dbcjckyee@gmail.com>
2015-12-21 19:05:04 -0800

    seed test database

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -3,9 +3,17 @@ Capybara.register_driver :selenium do |app|
   Capybara::Selenium::Driver.new(app, :browser => :chrome)
 end
 
-describe 'home page', js: true do
+describe 'home page' do
   it 'welcomes the user' do
     visit '/'
     page.should have_content('draft')
+  end
+end
+
+describe "Draft", js: true do
+  it "load the draft page successfully" do
+    visit '/'
+    click_on "Start draft"
+    expect(page).to have_content("Battle for Zendikar Draft")
   end
 end
