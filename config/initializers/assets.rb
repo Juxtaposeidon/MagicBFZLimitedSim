da100a1061310b686d2ad79a622e25b8cc2e3942
dbcjckyee <dbcjckyee@gmail.com>
2015-12-23 16:08:32 -0800

    start to style main page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,8 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( drafting/draftpage.js )
+Rails.application.config.assets.precompile += %w( mainpage/cycling.js )
+Rails.application.config.assets.precompile += %w( mainpage/cycle.js )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
