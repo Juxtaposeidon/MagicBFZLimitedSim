9632a08c03a6912f3a5d3d6cfeb9b0cda87f8c6f
dbcjckyee <dbcjckyee@gmail.com>
2015-12-23 18:02:12 -0800

    add sample seal pool generation

--- app/controllers/sample_controller.rb
+++ app/controllers/sample_controller.rb
@@ -1,6 +1,9 @@
 class SampleController < ApplicationController
   def new
-    @pack = Pack.new
+    @pack = []
+    6.times do
+      @pack << Pack.new.contents
+    end
+    @pack.flatten!.sort_by!{|card| [card.color, card.name]}
   end
-
 end
