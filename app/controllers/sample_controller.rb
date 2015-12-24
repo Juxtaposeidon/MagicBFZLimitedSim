235bff984c2cd90d9f3d605a3ce775a6b301a314
dbcjckyee <dbcjckyee@gmail.com>
2015-12-23 18:55:31 -0800

    fix sealed pool so that multicolor cards appear at the bottom

--- app/controllers/sample_controller.rb
+++ app/controllers/sample_controller.rb
@@ -4,6 +4,14 @@ class SampleController < ApplicationController
     6.times do
       @pack << Pack.new.contents
     end
-    @pack.flatten!.sort_by!{|card| [card.color, card.name]}
+    @pack.flatten!
+    @pack.sort_by!{|card| [card.color, card.name]}
+    @pack.each_with_index do |card,place|
+      if card.color2 != nil
+        placeholder = card
+        @pack.delete_at(place)
+        @pack.push(placeholder)
+      end
+    end
   end
 end
