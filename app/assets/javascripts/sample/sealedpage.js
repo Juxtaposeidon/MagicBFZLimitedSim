54af1e275b86fec3fa8cc0979dbad054932b900b
dbcjckyee <dbcjckyee@gmail.com>
2016-01-07 15:45:55 -0800

    start rebuilding sealed page in js

--- app/assets/javascripts/sample/sealedpage.js
+++ app/assets/javascripts/sample/sealedpage.js
@@ -1,17 +1,21 @@
 $(document).ready(function(){
   $(".sealedcards").on("click", "a", function(event){
     event.preventDefault();
-    var card = {
-      id: $(this).attr("id")
-    };
-    $.ajax({
-      data: card,
-      method: "GET",
-      url: '/sample/addcard'
-    })
-    .done(function(result){
-      $('.carddisplay').html(result['partial']);
-      $('#sealedpool ol').append(result['cardname'])
-    })
+    var card = $(this).attr('id');
+    var cardclass = $(this).attr('class')
+    $(this).toggle();
+    $('#sealedpool ol').append("<li><span class='"+ cardclass + "'>" + card + "</span></li>")
+    // var card = {
+    //   id: $(this).attr("id")
+    // };
+    // $.ajax({
+    //   data: card,
+    //   method: "GET",
+    //   url: '/sample/addcard'
+    // })
+    // .done(function(result){
+    //   $('.carddisplay').html(result['partial']);
+    //   $('#sealedpool ol').append(result['cardname'])
+    // })
   })
 })
