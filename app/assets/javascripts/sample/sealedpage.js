1b7a654adbb5381298530ac69d5dc2d19f57596f
dbcjckyee <dbcjckyee@gmail.com>
2016-01-07 17:04:46 -0800

    can now display card image when user hovers over card name in right side

--- app/assets/javascripts/sample/sealedpage.js
+++ app/assets/javascripts/sample/sealedpage.js
@@ -2,17 +2,30 @@ $(document).ready(function(){
   $(".sealedcards").on("click", "img", function(event){
     event.preventDefault();
     var card = $(this).data('name');
-    var cardid = $(this).attr('id');
+    var placeid = $(this).attr('id');
+    var cardid = $(this).data('cardid')
+    console.log(cardid)
     var cardclass = $(this).attr('class')
     $(this).toggle();
     $('#sealedpool ol').append("<li>" + card + " (-)</li>")
     $('li').last().addClass(cardclass)
-    $('li').last().attr('id', cardid)
+    $('li').last().attr('id', placeid)
+    $('li').last().data('cardid', cardid)
   })
 
   $('#sealedpool').on('click', 'li', function(){
     var cardid = $(this).attr('id');
     $(this).remove()
     $("#" + cardid).show();
+  })
+
+  $("#sealedpool").on("mouseover", "li", function(){
+    $("#cardhighlight").toggle();
+    $("#cardhighlight").html( "<img src=/assets/" + $(this).data("cardid") + ".jpg>")
+  })
+
+  $("#sealedpool").on("mouseout", "li", function(){
+    $("#cardhighlight").toggle();
+    $("#cardhighlight").html()
   })
 })
