43dd8ee149ac71d786723bb9814972045c933c12
dbcjckyee <dbcjckyee@gmail.com>
2016-01-08 13:54:00 -0800

    precompile assets for heroku

---
+++ public/assets/sealed/sealedpage-a57230e93e86ce2e2c54360068195d21887be52a6f93f29d16f24f0703b6f717.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  $(".sealedcards").on("click", "img", function(event){
+    event.preventDefault();
+    var card = $(this).data('name');
+    var placeid = $(this).attr('id');
+    var cardid = $(this).data('cardid')
+    console.log(cardid)
+    var cardclass = $(this).attr('class')
+    $(this).toggle();
+    $('#sealedpool ol').append("<li>" + card + " (-)</li>")
+    $('li').last().addClass(cardclass)
+    $('li').last().attr('id', placeid)
+    $('li').last().data('cardid', cardid)
+  })
+
+  $('#sealedpool').on('click', 'li', function(){
+    var cardid = $(this).attr('id');
+    $(this).remove()
+    $("#" + cardid).show();
+    $("#cardhighlight").hide();
+  })
+
+  $("#sealedpool").on("mouseover", "li", function(){
+    $("#cardhighlight").show();
+    $("#cardhighlight").html( "<img src=/assets/" + $(this).data("cardid") + ".jpg>")
+  })
+
+  $("#sealedpool").on("mouseout", "li", function(){
+    $("#cardhighlight").hide();
+    $("#cardhighlight").html()
+  })
+})
