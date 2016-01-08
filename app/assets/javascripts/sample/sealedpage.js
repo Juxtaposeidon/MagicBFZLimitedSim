fe4df612e521b4ffb3a595658dfaf1e8ebb5c315
dbcjckyee <dbcjckyee@gmail.com>
2016-01-07 16:11:44 -0800

    cards now disappear when clicked in sealed pool. working on getting the to reappear in left hand side

--- app/assets/javascripts/sample/sealedpage.js
+++ app/assets/javascripts/sample/sealedpage.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
   $(".sealedcards").on("click", "a", function(event){
     event.preventDefault();
-    var card = $(this).attr('id');
+    var card = $(this).data('name');
+    console.log(card)
     var cardclass = $(this).attr('class')
     $(this).toggle();
-    $('#sealedpool ol').append("<li><span class='"+ cardclass + "'>" + card + "</span></li>")
+    $('#sealedpool ol').append("<li><span class='"+ cardclass + " poolcard'>" + card + "</span></li>")
     // var card = {
     //   id: $(this).attr("id")
     // };
@@ -17,5 +18,12 @@ $(document).ready(function(){
     //   $('.carddisplay').html(result['partial']);
     //   $('#sealedpool ol').append(result['cardname'])
     // })
+  })
+
+  $('#sealedpool').on('click', '.poolcard', function(){
+    var card = $(this).text();
+    console.log(card)
+    $(this).closest("li").remove()
+    $("#" + card).show();
   })
 })
