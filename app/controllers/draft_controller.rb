eace76fff931718e454fa3f06e19ac5e8c7c9922
dbcjckyee <dbcjckyee@gmail.com>
2016-01-20 14:41:10 -0800

    clean up ruby variable names and some old code in controller files

--- app/controllers/draft_controller.rb
+++ app/controllers/draft_controller.rb
@@ -1,23 +1,23 @@
 class DraftController < ApplicationController
   def new
-    @@newdraft = Draft.new
-    @startingpack = @@newdraft.player1.currentpack.contents
+    @@draft = Draft.new
+    @startingpack = @@draft.player1.currentpack.contents
   end
 
   def update
     pickedcard = Card.find(params['cardid'])
-    @@newdraft.player1.addPool(pickedcard)
-    @@newdraft.player1.removeCard(pickedcard)
-    @@newdraft.cpuplayers.each {|player| player.cpuChoose}
-    @@newdraft.rotatePacks
-    if @@newdraft.player1.pool.length % 14 == 0
-      @@newdraft.newPacks
+    @@draft.player1.addPool(pickedcard)
+    @@draft.player1.removeCard(pickedcard)
+    @@draft.cpuplayers.each {|player| player.cpuChoose}
+    @@draft.rotatePacks
+    if @@draft.player1.pool.length % 14 == 0
+      @@draft.newPacks
     end
   end
 
   def index
-    @pack = @@newdraft.player1.currentpack.contents
-    pickedcard=@@newdraft.player1.pool.last
+    @pack = @@draft.player1.currentpack.contents
+    pickedcard=@@draft.player1.pool.last
     render :json => {
       :cardname => "<span class = 'card #{pickedcard.color} #{pickedcard.color2}' id = '#{pickedcard.id}'>" + pickedcard.name + "</span>",
       :partial => render_to_string(:partial => 'draft/draft')
