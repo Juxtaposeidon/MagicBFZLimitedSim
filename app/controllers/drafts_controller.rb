22b1f2b98dc5d880a53b17f74fb8b6ae73e8ab04
dbcjckyee <dbcjckyee@gmail.com>
2016-01-21 19:58:26 -0800

    redirect people to drafts/new when they attempt to access /drafts via http

--- app/controllers/drafts_controller.rb
+++ app/controllers/drafts_controller.rb
@@ -16,12 +16,16 @@ class DraftsController < ApplicationController
   end
 
   def index
-    @pack = @@draft.player1.currentpack.contents
-    pickedcard=@@draft.player1.pool.last
-    render :json => {
-      :cardname => "<span class = 'card #{pickedcard.color} #{pickedcard.color2}' id = '#{pickedcard.id}'>" + pickedcard.name + "</span>",
-      :partial => render_to_string(:partial => 'drafts/draft')
-    }
+    if request.xhr?
+      @pack = @@draft.player1.currentpack.contents
+      pickedcard=@@draft.player1.pool.last
+      render :json => {
+        :cardname => "<span class = 'card #{pickedcard.color} #{pickedcard.color2}' id = '#{pickedcard.id}'>" + pickedcard.name + "</span>",
+        :partial => render_to_string(:partial => 'drafts/draft')
+      }
+    else
+      redirect_to new_draft_path
+    end
   end
 
 end
