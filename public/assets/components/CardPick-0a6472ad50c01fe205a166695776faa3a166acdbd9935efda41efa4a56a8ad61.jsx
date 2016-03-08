354629d4adbfb9f4655c1e532c9f3ee9cdb374e4
dbcjckyee <dbcjckyee@gmail.com>
2016-03-07 23:07:04 -0800

    compile assets

---
+++ public/assets/components/CardPick-0a6472ad50c01fe205a166695776faa3a166acdbd9935efda41efa4a56a8ad61.jsx
@@ -0,0 +1,17 @@
+var CardPick = React.createClass({
+  onEnterName: function(){
+    this.props.onMouseEnter(this.props.id)
+  },
+
+  onClickSubmit: function(){
+    if(this.props.onClick!= undefined){
+      this.props.onClick(this.props)
+    }
+  },
+
+  render: function(){
+    return(
+      <span className={this.props.colors} id={this.props.id} onClick={this.onClickSubmit} onMouseEnter={this.onEnterName} onMouseLeave={this.props.onMouseLeave}>[{this.props.name}]{this.props.minus}</span>
+    )
+  }
+})
