987ea2668296fe91bf4bd50195b9d38b09214242
dbcjckyee <dbcjckyee@gmail.com>
2016-03-21 21:24:32 -0700

    fix up js styling in react components

--- app/assets/javascripts/components/CardPick.js.jsx
+++ app/assets/javascripts/components/CardPick.js.jsx
@@ -1,17 +1,25 @@
 var CardPick = React.createClass({
-  onEnterName: function(){
+  onEnterName: function () {
     this.props.onMouseEnter(this.props.id)
   },
 
-  onClickSubmit: function(){
-    if(this.props.onClick!= undefined){
+  onClickSubmit: function () {
+    if (this.props.onClick!= undefined) {
       this.props.onClick(this.props)
     }
   },
 
-  render: function(){
-    return(
-      <span className={this.props.colors} id={this.props.id} onClick={this.onClickSubmit} onMouseEnter={this.onEnterName} onMouseLeave={this.props.onMouseLeave}>[{this.props.name}]{this.props.minus}</span>
+  render: function () {
+    return (
+      <span
+        className={this.props.colors}
+        id={this.props.id}
+        onClick={this.onClickSubmit}
+        onMouseEnter={this.onEnterName}
+        onMouseLeave={this.props.onMouseLeave}
+      >
+        [{this.props.name}]{this.props.minus}
+      </span>
     )
   }
 })
--- app/assets/javascripts/components/Draftpool.js.jsx
+++ app/assets/javascripts/components/Draftpool.js.jsx
@@ -1,44 +1,44 @@
 var counter = 0
 var DraftpoolContainer = React.createClass({
-  getInitialState: function(){
-    return{
+  getInitialState: function () {
+    return {
       cards: this.props.cards,
-      pool: [],
+      pool: []
     }
   },
 
-  getCards: function(pick){
+  getCards: function (pick) {
     var component = this
     var nospam = 0
-    if(nospam==0){
+    if (nospam==0) {
       nospam = 1
       $.ajax({
         url: '/drafts/update',
         method: 'PUT',
         data: {'cardid': pick.id}
       })
-      .done(function(){
+      .done(function () {
         $.ajax({
           url: '/drafts',
           method: "GET"
         })
-        .done(function(result){
+        .done(function (result) {
           nospam = 0
           component.setState({
             cards: result['pack'],
             pool: component.state.pool.concat(result['pick'])
           })
           counter++;
-          if(counter == 42){
-            $('.cardpacksection').html("<H3>The draft is now over</H3>")
+          if (counter == 42) {
+            $(".cardpacksection").html("<H3>The draft is now over</H3>")
           }
         })
       })
     }
   },
 
-  render: function(){
-    return(
+  render: function () {
+    return (
       <Draftpool
         cards={this.state.cards}
         pool={this.state.pool}
@@ -49,23 +49,23 @@ var DraftpoolContainer = React.createClass({
 })
 
 var Draftpool = React.createClass({
-  getInitialState: function(){
-    return{
+  getInitialState: function () {
+    return {
       highlightedcard: undefined
     }
   },
 
-  highlightCard: function(card){
+  highlightCard: function (card) {
     this.setState({highlightedcard: "/assets/" + card})
   },
 
-  hideCard: function(thing){
+  hideCard: function () {
     this.setState({highlightedcard: undefined})
   },
 
-  render: function(){
-    var cards = this.props.cards.map(function(item){
-      return(
+  render: function () {
+    var cards = this.props.cards.map(function (item) {
+      return (
         <Card
           onClick={this.props.picking}
           key={item.id}
@@ -74,8 +74,8 @@ var Draftpool = React.createClass({
         />
       )
     }, this)
-    var wholepool = this.props.pool.map(function(item){
-      return(
+    var wholepool = this.props.pool.map(function (item) {
+      return (
         <CardPick
           id={item.id}
           name={item.name}
@@ -89,37 +89,37 @@ var Draftpool = React.createClass({
     var pool2 = wholepool.slice(14,28)
     var pool3 = wholepool.slice(28,42)
 
-    if(this.state.highlightedcard){
+    if (this.state.highlightedcard) {
       var highlighted= this.state.highlightedcard
     }
     return (
       <div>
-        <div className = 'cards'>
-        <div id = 'highlightedcard'><img src={highlighted}/></div>
+        <div className = "cards">
+        <div id = "highlightedcard"><img src={highlighted}/></div>
           <h3>Battle for Zendikar Draft</h3>
           <br />
-          <div className = 'cardpacksection'>
+          <div className = "cardpacksection">
             {cards}
           </div>
         </div>
-        <div id='pool'>
-          <table id ='cardpooltable'>
+        <div id="pool">
+          <table id ="cardpooltable">
           <tbody>
             <tr>
-              <td className = 'pooltd'>
+              <td className = "pooltd">
                 <h3 id='pooltitle'>Card pool</h3>
                 <h4>Pack 1</h4>
                 {pool1}
               </td>
             </tr>
             <tr>
-              <td className = 'pooltd'>
+              <td className = "pooltd">
                 <h4>Pack 2</h4>
                 {pool2}
               </td>
             </tr>
             <tr>
-              <td className = 'pooltd'>
+              <td className = "pooltd">
                 <div>
                   <h4>Pack 3</h4>
                   {pool3}
